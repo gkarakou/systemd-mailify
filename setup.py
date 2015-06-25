607f5cc2036a9ee941349e50d2252e939aa8c8c3
George Karakougioumtzis <gkarakou@gmail.com>
2015-06-25 23:34:36 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python2
+
+# Copyright (C) 2015 George Karakou (gkarakou)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You can get a copy of the GNU General Public License at
+# <http://www.gnu.org/licenses/>.
+
+#http://stackoverflow.com/questions/11536764/attempted-relative-import-in-non-package-even-with-init-py
+#if __package__ is None:
+#    import sys
+#    from os import path
+#    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+#    from install_script import Installer
+#else:
+#    from .install_script import Installer
+from distutils.core import setup
+
+setup(
+name = 'systemd-mailify',
+version = '1.0',
+description = 'Provides mailing notifications for failed systemd services',
+long_description = 'A python based service that mail-ifies failed systemd services',
+author = 'George Karakougioumtzis <gkarakou>',
+author_email = 'gkarakou@gmail.com',
+url = 'https://github.com/gkarakou/systemd-mailify',
+platforms = 'linux',
+license = 'GPL-3.0',
+packages = ['systemd-mailify'],
+package_data = {'systemd-mailify': ['conf/*']},
+install_requires= ['python-systemd'],
+classifiers = ['Development Status :: 1.0 - Stable',
+'Environment :: Server',
+'Intended Audience :: System Administrators',
+'License :: GPL-3.0 ',
+'Operating System :: Linux',
+'Programming Language :: Python2.7'],
+data_files = [('/etc', ['systemd-mailify/conf/systemd-mailify.conf']), ('/usr/lib/systemd/system', ['systemd-mailify/conf/systemd-mailify.service'])],
+scripts = ['scripts/systemd-mailify.py']
+)
